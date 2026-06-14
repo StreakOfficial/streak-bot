cb0f1da5da212fcffa41f2ba67a774bbb0f6b543
Streak Official <20pritchekaitlin@gmail.com>
2026-06-14 15:42:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,21 +8,23 @@ app.use(express.json());
 
 const FILE = "./data.json";
 
+/* -------- READ DATA -------- */
 function getData() {
   if (!fs.existsSync(FILE)) return {};
   return JSON.parse(fs.readFileSync(FILE, "utf8"));
 }
 
+/* -------- SAVE DATA -------- */
 function saveData(data) {
   fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
-// GET
+/* -------- GET -------- */
 app.get("/api/settings", (req, res) => {
   res.json(getData());
 });
 
-// POST
+/* -------- POST -------- */
 app.post("/api/settings", (req, res) => {
   const updated = { ...getData(), ...req.body };
   saveData(updated);
